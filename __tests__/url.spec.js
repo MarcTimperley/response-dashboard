094d84d212482843e0c5d880323e77a21cd113fc
MarcTimperley <marctimperley@yahoo.co.uk>
2020-04-07 16:31:54 +0100

    fix: updated test cases

--- __tests__/url.spec.js
+++ __tests__/url.spec.js
@@ -3,17 +3,20 @@ describe('response', () => {
   test('simple proxy', async() => {
     const url = 'url=http://designs94.com'
     const response = await proxy(url)
+    console.log(response)
     expect(response.response).toBeGreaterThan(15)
     expect(response.url).toEqual(url.replace('url=', ''))
   })
   test('simple proxy (no url)', async() => {
     const response = await proxy()
+    console.log(response)
     expect(response.response).toBeGreaterThan(15)
     expect(response.url).toEqual('http://www.example.com')
   })
   test('error condition', async() => {
     const url = 'url=http://designs94.comxxx'
     const response = await proxy(url)
+    console.log(response)
     expect(response.response.code).toMatch('ENOTFOUND')
   })
 })
